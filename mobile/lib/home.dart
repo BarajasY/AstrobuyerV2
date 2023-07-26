e9bdb41e6580f1fbd11ea5a01dd88ae9f7b60138
Yahir Moreno Barajas <100240827+BarajasY@users.noreply.github.com>
2023-07-26 00:30:57 -0600

    http request. Gradient applied.

--- mobile/lib/home.dart
+++ mobile/lib/home.dart
@@ -11,35 +11,61 @@ class Home extends StatefulWidget {
 }
 
 class _Home extends State<Home> {
-  late Future<Astro> astros;
+  late Future<List<dynamic>> astros;
 
-  Future<Astro> getAstros() async {
-    final response = await http.get(Uri.parse("http://localhost:8000/astros"));
+  Future<List<Astro>> getAstros() async {
+    List<Astro> astros = [];
+    Uri uri = Uri.http("10.0.2.2:8000", "/astros");
+
+    final response = await http.get(uri);
     if (response.statusCode == 200) {
-      return Astro.fromJson(jsonDecode(response.body));
+      var decoded = json.decode(response.body);
+      for (var element in decoded) {
+        Astro astro = Astro(
+            id: element["id"],
+            name: element["name"],
+            price: element["price"],
+            category: element["category"],
+            temperature: element["temperature"],
+            image: element["image"]);
+        astros.add(astro);
+      }
+      return astros;
     } else {
       throw Exception("Failed to fetch astros.");
     }
   }
 
   @override
-  void initState() {
-    super.initState();
-    astros = getAstros();
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFF000000),
-      body: Center(child: FutureBuilder<Astro>(builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          return Text(snapshot.data!.name);
-        } else if (snapshot.hasError) {
-          return Text('${snapshot.error}');
-        }
-        return const Text("Loading...");
-      })),
+      body: Center(
+          child: Container(
+              decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                colors: [Color(0xff000000), Color(0xff042b4a)],
+                stops: [0.6, 1],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              )),
+              child: FutureBuilder(
+                  future: getAstros(),
+                  builder: (BuildContext ctx, AsyncSnapshot snapshot) {
+                    if (snapshot.data == null) {
+                      return const CircularProgressIndicator();
+                    } else {
+                      return ListView.builder(
+                        itemCount: snapshot.data.length,
+                        itemBuilder: (ctx, index) => Text(
+                          snapshot.data[index].name,
+                          style: const TextStyle(
+                              color: Color(0xFFFFFFFF),
+                              fontWeight: FontWeight.w900),
+                        ),
+                      );
+                    }
+                  }))),
     );
   }
 }
